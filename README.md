58a764fed3bff9f1f87944bdc224d2a3c2b50faa
TechLadySheba <jehoshebaj@gmail.com>
2023-09-08 06:59:16 -0700

    my first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # HNGx
+Here are my codes for my first HNGx internship task
+The code gives a webpage look of my HNGx profile outlook with a UTC
---
+++ Task/Task.js
@@ -0,0 +1,15 @@
+// Add JavaScript code here to update the current day of the week and UTC time if needed
+document.addEventListener("DOMContentLoaded", 
+function () {
+  const dayOfWeekElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+  const utcTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
+  
+  const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const currentDate = new Date();
+  
+  const dayOfWeek = daysOfWeek[currentDate.getUTCDay()];
+  const utcTime = currentDate.toISOString().split('T')[1].split('.')[0] + " UTC";
+  
+  dayOfWeekElement.textContent = dayOfWeek;
+  utcTimeElement.textContent = utcTime;
+});
