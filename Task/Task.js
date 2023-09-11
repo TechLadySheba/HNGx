38e6b036ad5591f70ef592d9b0bf529735175c84
TechLadySheba <jehoshebaj@gmail.com>
2023-09-10 17:50:46 -0700

    updated css

--- Task/Task.js
+++ Task/Task.js
@@ -1,15 +1,14 @@
-
-document.addEventListener("DOMContentLoaded", 
-function () {
-  const dayOfWeekElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
-  const utcTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
-  
-  const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  const currentDate = new Date();
-  
-  const dayOfWeek = daysOfWeek[currentDate.getUTCDay()];
-  const utcTime = currentDate.toISOString().split('T')[1].split('.')[0] + " UTC";
-  
-  dayOfWeekElement.textContent = dayOfWeek;
-  utcTimeElement.textContent = utcTime;
-});
+    document.addEventListener("DOMContentLoaded", 
+    function () {
+      const dayOfWeekElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+      const utcTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
+      
+      const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+      const currentDate = new Date();
+      
+      const dayOfWeek = daysOfWeek[currentDate.getUTCDay()];
+      const utcTime = currentDate.toISOString().split('T')[1].split('.')[0] + " UTC";
+      
+      dayOfWeekElement.textContent = dayOfWeek;
+      utcTimeElement.textContent = utcTime;
+    })
